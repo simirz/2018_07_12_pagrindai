b10a50c2a3506b9e1d6fd22f442e3a5671d3c712
lenovo pc <lenovo pc@DESKTOP-PBFSF28>
2018-07-16 21:01:32 +0300

    pagalba Onai

---
+++ src/days/third/Calculator.java
@@ -0,0 +1,21 @@
+package days.third;
+
+public class Calculator {
+    public double sum(double numb, double numb2) {
+        return numb + numb2;
+    }
+
+    public double substraction(double numb, double numb2) {
+        return numb - numb2;
+    }
+    public double division(double numb, double numb2) {
+        return numb / numb2;
+    }
+    public double multiplication(double numb, double numb2) {
+        return numb * numb2;
+    }
+    public double pow(double numb, double numb2) {
+        return Math.pow(numb, numb2);
+    }
+
+}
